e30d41257041002ec7f3628521d437e62938a7b5
fvpetrov <51488124+fvpetrov@users.noreply.github.com>
2020-04-25 16:16:49 +0200

    Added: MySensors configuration.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
 
-void setup() {
+
+// Enable debug prints to serial monitor
+#define MY_DEBUG
+
+
+// Enable and select radio type attached
+//#define MY_RADIO_RF24
+#define MY_RADIO_NRF5_ESB
+//#define MY_RADIO_RFM69
+//#define MY_RADIO_RFM95
+
+// Enable repeater functionality for this node
+//#define MY_REPEATER_FEATURE
+
+// Smart sleep delay, to allow the controller send any buffered messages
+// Use a small value to save battery.
+#define MY_SMART_SLEEP_WAIT_DURATION_MS	50
+
+#include <MySensors.h>
+
+// End of MySensors configuration
+// -----------------------------------------------------------------------------
+
+
+void setup()
+{
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
